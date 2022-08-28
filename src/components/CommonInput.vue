088fe463726e78277745412d428f48927f0875fb
함수원 <dev.code.su@gmail.com>
2022-08-28 22:23:43 +0900

    [ADD] input 분리 - debounce

---
+++ src/components/CommonInput.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps(['modelValue', 'type']);
+const emits = defineEmits(['update:modelValue']);
+
+const valueEmit = (function () {
+  let inDebounce = null;
+
+  return function (ev) {
+    clearTimeout(inDebounce);
+    inDebounce = setTimeout(() => {
+      emits('update:modelValue', ev.target.value);
+    }, 700);
+  };
+})();
+</script>
+
+<template>
+  <input :type="props.type" :value="modelValue" @input="valueEmit" />
+</template>
