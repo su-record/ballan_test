22f0f3ef5f6ad705ad89ecd8985d607cf38d3316
함수원 <dev.code.su@gmail.com>
2022-08-28 22:25:37 +0900

    [MOD] model 추가

--- src/composables/useSignUpPanel.js
+++ src/composables/useSignUpPanel.js
@@ -2,8 +2,7 @@ import InputString from '@/components/InputString.vue';
 import InputAddress from '@/components/InputAddress.vue';
 import InputCard from '@/components/InputCard.vue';
 
-export const useSignUpPanel = (step = 0) => {
-  console.log('???? useSignPanel step', step);
+export default function (step = 0) {
   switch (step) {
     case 1:
       return [
@@ -11,15 +10,18 @@ export const useSignUpPanel = (step = 0) => {
           label: '이름',
           component: InputString,
           type: 'text',
+          model: 'name',
         },
         {
           label: '연락처',
           component: InputString,
           type: 'tel',
+          model: 'cellular',
         },
         {
           label: '주소',
           component: InputAddress,
+          model: 'address',
         },
       ];
     case 2:
@@ -27,6 +29,7 @@ export const useSignUpPanel = (step = 0) => {
         {
           label: '카드번호',
           component: InputCard,
+          model: 'cardNo',
         },
       ];
     case 0:
@@ -36,17 +39,20 @@ export const useSignUpPanel = (step = 0) => {
           label: '이메일',
           component: InputString,
           type: 'email',
+          model: 'email',
         },
         {
           label: '비밀번호',
           component: InputString,
           type: 'password',
+          model: 'password',
         },
         {
           label: '비밀번호 확인',
           component: InputString,
           type: 'password',
+          model: 'pwRetry',
         },
       ];
   }
-};
+}
