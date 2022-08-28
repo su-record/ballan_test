7cb04e0b0d71011d6a220da51f89ded75f3288ff
함수원 <dev.code.su@gmail.com>
2022-08-29 02:21:01 +0900

    [ADD] 네비게이션 가드. 데이터 없을 시 화면 전환

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
+import { useAccountStore } from '@/stores/account';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      redirect: "/sign-up",
+      path: '/',
+      name: 'root',
+      redirect: '/sign-up',
     },
     {
-      path: "/sign-up",
-      name: "signUp",
-      component: () => import("../views/SignUpView.vue"),
+      path: '/sign-up',
+      name: 'SignUp',
+      component: () => import('../views/SignUpView.vue'),
     },
     {
-      path: "/registration-complete",
-      name: "registrationComplete",
-      component: () => import("../views/RegistrationCompleteView.vue"),
+      path: '/registration-complete',
+      name: 'RegistrationComplete',
+      component: () => import('../views/RegistrationCompleteView.vue'),
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/registration-complete') {
+    const account = useAccountStore();
+    if (!account.info) next({ name: 'SignUp' });
+  }
+
+  next();
 });
 
 export default router;
