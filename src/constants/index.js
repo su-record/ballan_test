153c091a8d43167f4a52bec8e859df6aead444cc
함수원 <dev.code.su@gmail.com>
2022-08-29 02:20:41 +0900

    [ADD] 정규식

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,4 +5,6 @@ export const REG_EXR = {
   PASSWORD: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}$/,
   NAME: /([가-힣]{2,}|[a-zA-Z]{3,})$/,
   NUMBER: /\d+$/,
+  REPLACE_NUMBER: /[^0-9]/g,
+  REPLACE_CELLULAR: /^(\d{2,3})(\d{3,4})(\d{4})$/,
 };
