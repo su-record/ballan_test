67803a19cf7edbe743be2e5bf208dd218efe7b96
함수원 <dev.code.su@gmail.com>
2022-08-28 22:25:18 +0900

    [MOD] input 오류 메세지 출력

--- src/components/InputContainer.vue
+++ src/components/InputContainer.vue
@@ -1,8 +1,13 @@
 <script setup>
-const props = defineProps(['list']);
+const props = defineProps(['list', 'validation']);
 </script>
 <template>
   <div class="input-container">
-    <component v-for="input in props.list" :is="input.component" :info="input" />
+    <component
+      v-for="input in props.list"
+      :is="input.component"
+      :info="input"
+      :isValid="validation[input.model]"
+    />
   </div>
 </template>
--- src/components/InputString.vue
+++ src/components/InputString.vue
@@ -1,9 +1,18 @@
 <script setup>
-const props = defineProps(['info']);
+import { inject } from 'vue';
+import CommonInput from './CommonInput.vue';
+
+const props = defineProps(['info', 'isValid']);
+const form = inject('form');
 </script>
 <template>
   <div class="input-item">
-    <label for="">{{ props.info.label }}</label>
-    <input type="text" />
+    <label>{{ props.info.label }}</label>
+    <div class="input-wrap">
+      <CommonInput v-model="form[props.info.model]" :type="props.info.type" />
+      <span v-if="form[props.info.model] && !props.isValid" class="validation-message">
+        다시 입력해주세요.
+      </span>
+    </div>
   </div>
 </template>
